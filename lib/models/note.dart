d01af7e5bc292a91fa5a3c53d6f9c0a41940add3
เคเบิล <cbkcontactmail@gmail.com>
2024-11-12 22:47:17 +0700

    Update

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,5 +1,5 @@
 import 'package:isar/isar.dart';
 
 class Note {
-  Id id = Isar.autoIncrement;
+  //Id id = Isar.autoIncrement;
 }
--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
 
+class NotesPage extends StatefulWidget {
+  const NotesPage({super.key});
+
+  @override
+  State<NotesPage> createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  // เพิ่ม create note ui 20241112
+  void createNote() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              content: TextField(
+                controller: TextEditingController(),
+              ),
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Notes')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNote,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
